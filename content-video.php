1c826e0c00ca403fcdb82d935f30688facd478fb
heroesaddict <chattokim@gmail.com>
2018-02-25 21:09:53 +0800

    Part10

--- content-video.php
+++ content-video.php
@@ -1,6 +1,9 @@
-<article id = "post-<?php the_ID(); ?>"   <?php post_class();?>	
-	<h3>Video Post:<?php the_title(); ?></h3>
-	<div class="thumbnail-img"><?php the_post_thumbnail('thumbnail'); ?></div>
-	<small>Posted on:<?php the_time('F j,Y'); ?> at <?php the_time('g:i a'); ?>, in <?php the_category(); ?></small>
+<article id = "post-<?php the_ID(); ?>"   <?php post_class();?>>
+	<header class="entry-header">
+	    <?php the_title(sprintf('<h1 class="entry-title"><a href="%s">',esc_url(get_permalink()) ),'</a></h1>'); ?>
+	    <small>Posted on:<?php the_time('F j,Y'); ?> at <?php the_time('g:i a'); ?>, in <?php the_category(); ?></small>
+	</header>
+	<div class="thumbnail"><?php the_post_thumbnail('medium'); ?></div>
+	
 </article>
 <hr>
--- page-home.php
+++ page-home.php
@@ -1,22 +1,39 @@
 <?php get_header(); ?>
 
 <div class="row">
-	<div class="col-xs-12 col-sm-12">
 		<?php 
 
-			$lastBlog = new WP_Query('type=post&posts_per_page=1');
+		$args_cat = array(
+			'include' => '7,8,9'
+		);
 
-			if( $lastBlog->have_posts() ):
-			while( $lastBlog->have_posts()): $lastBlog->the_post(); ?>
+		$categories = get_categories($args_cat);
+		/*var_dump($categories);*/
+		foreach($categories as $category): 
 
-				<?php get_template_part('content',get_post_format()); ?>
+			$args = array(
+				'type' =>'post',
+				'posts_per_page' => '1',
+				'category__in' => $category->term_id,
+				'category__not_in' => array(10,1)
+			);
 
-			<?php endwhile;
-			endif;
-			wp_reset_postdata();
+				$lastBlog = new WP_Query($args);
+
+				if( $lastBlog->have_posts() ):
+				while( $lastBlog->have_posts()): $lastBlog->the_post(); ?>
+					<div class="col-xs-12 col-sm-4">
+						<?php get_template_part('content','featured'); ?>
+					</div>
+				<?php endwhile;
+				endif;
+				wp_reset_postdata();
+
+		endforeach;
 		?>
-	</div>
+</div>
 
+<div class="row">
 	<div class="col-xs-12 col-sm-8">
 		<?php 
 		if(have_posts()):
@@ -28,7 +45,7 @@
 		endif;
 
 		//Print other 2 posts and not the first one
-		$args = array(
+/*		$args = array(
 			'type' => 'post',
 			'posts_per_page' => 2,
 			'offset' => 1,
@@ -44,14 +61,14 @@
 			<?php endwhile;
 			endif;
 			wp_reset_postdata();
-
+*/
 		?>
 
-		<hr>
-		<h2>Tutorials:</h2>
+	<!-- 	<hr> -->
+		<!-- <h2>Tutorials:</h2> -->
 
 		<?php 
-		//Print only tutorials posts
+		/*//Print only tutorials posts
 		$lastBlog = new WP_Query('type=post&posts_per_page=-1&category_name=tutorials');
 
 			if( $lastBlog->have_posts() ):
@@ -61,7 +78,7 @@
 
 			<?php endwhile;
 			endif;
-			wp_reset_postdata();
+			wp_reset_postdata();*/
 
 
 
