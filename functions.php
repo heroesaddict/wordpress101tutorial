94b5df52476c383fc88f76a2063a001ed0865095
heroesaddict <chattokim@gmail.com>
2018-03-18 12:18:22 +0800

    Part20 & Part21

--- functions.php
+++ functions.php
@@ -131,7 +131,7 @@ add_filter('the_generator','wordpress101_remove_version');
 			'thumbnail',
 			'revisions',
 		),
-		'taxonomies' => array('category', 'post_tag'),
+		//'taxonomies' => array('category', 'post_tag'),
 		'menu_position' => 5,
 		'exclude_from_search' => false
 	);
@@ -140,3 +140,47 @@ add_filter('the_generator','wordpress101_remove_version');
 add_action('init','wordpress101_custom_post_type');
 
 flush_rewrite_rules();
+
+/* ===============================
+   Custom Taxonomies
+   ===============================
+*/
+   function wordpress101_custom_taxonomies (){
+   		//add new taxonomy hierarchical
+	$labels = array(
+		'name' => 'Fields',
+		'singular_name' => 'Field',
+		'search_items' => 'Search Fields',
+		'all_items' => 'All Fields',
+		'parent_item' => 'Parent Field',
+		'parent_item_colon' => 'Parent Field:',
+		'edit_item' => 'Edit Field',
+		'update_item' => 'Update Field',
+		'add_new_item' => 'Add New Work Field',
+		'new_item_name' => 'New Field Name',
+		'menu_name' => 'Fields'
+	);
+	
+	$args = array(
+		'hierarchical' => true,
+		'labels' => $labels,
+		'show_ui' => true,
+		'show_admin_column' => true,
+		'query_var' => true,
+		'rewrite' => array( 'slug' => 'field' )
+	);
+	
+	register_taxonomy('field', array('portfolio'), $args);
+	
+	//add new taxonomy NOT hierarchical
+	
+	register_taxonomy('software', 'portfolio', array(
+		'label' => 'Software',
+		'rewrite' => array( 'slug' => 'software' ),
+		'hierarchical' => false
+	) );
+}
+add_action( 'init' , 'wordpress101_custom_taxonomies' );
+
+
+	
--- single-portfolio.php
+++ single-portfolio.php
@@ -11,7 +11,21 @@
 				<?php if(has_post_thumbnail()): ?>
 					<div class="pull-right"><?php the_post_thumbnail('thumbnail'); ?></div>
 				<?php endif; ?>
-				<small><?php the_category(' '); ?> || <?php the_tags(); ?> || <?php edit_post_link(); ?></small>
+				<small><?php 
+				$terms_list = wp_get_post_terms( $post->ID,'field');
+				$i = 0;
+					foreach ($terms_list as $term) {$i++;
+						if($i>1){ echo ', '; }
+						echo $term->name." ";
+					}
+				 ?> || <?php 
+				$terms_list = wp_get_post_terms( $post->ID,'software');
+				$i = 0;
+					foreach ($terms_list as $term) {$i++;
+						if($i>1){ echo ', '; }
+						echo $term->name." ";
+					}
+				 ?>  || <?php edit_post_link(); ?></small>
 				<?php the_content(); ?>
 				<hr>
 				<div class="row">
